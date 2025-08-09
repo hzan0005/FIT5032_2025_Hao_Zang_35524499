810290d08be8cf7c7e3ca0cf23a12562f0710641
hzan0005 <hzan0005@student.monash.edu>
2025-08-09 19:38:20 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
+// vite.config.ts / vite.config.js
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/FIT5032_2025_Hao_Zang_35524499/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
